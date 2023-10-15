474fc8479503fa328e210a8e1ae1492c255c7643
Mateus <mateushbsb@gmail.com>
2023-10-15 18:02:17 -0300

    UpdateDependencies

--- src/main/java/br/com/mateussilva/todolist/errors/ExceptionHandlerController.java
+++ src/main/java/br/com/mateussilva/todolist/errors/ExceptionHandlerController.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
-  
-  @ExceptionHandler(HttpMessageNotReadableException.class)
-  public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
-    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
-  }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(e.getMostSpecificCause().getMessage());
+    }
 }
